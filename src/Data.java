e0de32fb4d742e2f0cd258bfca35956c8462f448
= <=>
2018-12-22 14:49:41 +0900

    javaの絵本p9

---
+++ src/Data.java
@@ -0,0 +1,18 @@
+
+public class Data {
+	public static void main(String[] args){
+		int a;
+		int b;
+		int c;
+		a = 60;
+		b = 90;
+		c = 30;
+		a = c = b;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		
+	}
+}
---
+++ src/Print.java
@@ -0,0 +1,7 @@
+
+public class Print {
+	public static void main(String[] args){
+		System.out.print("Hello world!") ;
+		System.out.print("Hello world!") ;
+	}
+}
