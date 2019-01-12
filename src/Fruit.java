8acf4a15e136c7c41f3d86a37f7da1e837efb3f6
M-yuito <matsumoto.yuto.99@gmail.com>
2019-01-12 14:48:57 +0900

    javaの絵本p12

---
+++ src/Fruit.java
@@ -0,0 +1,11 @@
+
+public class Fruit {
+	public static void main(String[] args) {
+		String apple = "‚è‚ñ‚²";
+		long a = 1234567890l;
+		
+		String fruit = apple + a + "‚±";
+		System.out.println(fruit);
+	}
+
+}
---
+++ src/Number.java
@@ -0,0 +1,8 @@
+
+public class Number {
+	public static void main(String[] args) {
+		
+	}
+	
+
+}
